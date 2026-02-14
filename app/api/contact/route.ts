fd696787e3f10eb06d2dda78e27d5d6fc1a9d268
Olivia Henderson <olivia.praha01@gmail.com>
2026-02-14 15:59:56 +0100

    Add contact API route (Gmail OAuth) + deps

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import nodemailer from "nodemailer";
+import * as nodemailer from "nodemailer";
 import { google } from "googleapis";
 
 function clean(v: unknown) {
